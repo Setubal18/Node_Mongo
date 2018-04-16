711e7889133fcb80ddfd01a8e69b9bf231a36965
Leonardo Setubal Maggio <setubal43@gmail.com>
2018-04-16 16:05:51 -0300

    segundo commit. nao ouve grantes auterações

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 //Conexao com bd simples.
 // var MongoClient = require('mongodb').MongoClient;
 //
@@ -11,5 +10,19 @@
 //     console.log("Conexao estabelecida com sucesso.");
 //
 //   db.close();
-// });
+// };
+
+var MongoClient = require('mongodb').MongoClient;
+
+var servidor = 'mongodb://localhost:27017/db_teste';
+
+MongoClient.connect(servidor, function (erro, db) {
+    if (erro)
+        console.log("Erro ao estabelecer conexao:" + erro);
+    else
+        console.log("Conexao estabelecida com sucesso.");
+
+    db.close();
+};
+
 
